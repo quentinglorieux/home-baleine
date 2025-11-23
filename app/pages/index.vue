0e9e4e6eceb6e6860df67ef745ff37f951a48e69
Quentin Glorieux <quentin.glorieux@gmail.com>
2025-11-23 13:33:08 +0100

    update app/pages/index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -15,7 +15,7 @@
       <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
         <UCard
           as="a"
-          href="https://co2.baleine.lkb.upmc.fr"
+          href="http://co2.baleine.lkb.upmc.fr"
           target="_blank"
           rel="noopener noreferrer"
           variant="outline"
@@ -44,7 +44,7 @@
 
         <UCard
           as="a"
-          href="https://intranet.baleine.lkb.upmc.fr"
+          href="http://intranet.baleine.lkb.upmc.fr"
           target="_blank"
           rel="noopener noreferrer"
           variant="outline"
@@ -73,7 +73,7 @@
 
         <UCard
           as="a"
-          href="https://booking.baleine.lkb.upmc.fr"
+          href="http://booking.baleine.lkb.upmc.fr"
           target="_blank"
           rel="noopener noreferrer"
           variant="outline"
